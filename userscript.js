517d946871bd5d479213f6ca78c01547049c9b43
headassbtw <cooldood909@gmail.com>
2022-06-23 04:10:05 -0700

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name          DDR Twitter
+// @version       0.0.1
+// @description   Replaces icons with DDR arrows
+// @author        headassbtw
+// @license       MIT
+// @match         https://twitter.com/*
+// @exclude       https://twitter.com/i/cards/*
+// @exclude       https://twitter.com/i/release_notes
+// @exclude       https://twitter.com/*/privacy
+// @exclude       https://twitter.com/*/tos
+// @exclude       https://twitter.com/account/access
+// @resource      css https://github.com/headassbtw/twitter/raw/master/style/main.css
+// @updateURL     https://github.com/headassbtw/twitter/raw/master/userscript.js
+// @downloadURL   https://github.com/headassbtw/twitter/raw/master/userscript.js
+// ==/UserScript==
